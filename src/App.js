b3691c020ef30cb6613ea5c767cd89fe1892fc44
Gnvd21 <giodagoro@gmail.com>
2024-03-14 23:09:32 +0800

    Beta 1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-/*import './App.css';
+import './App.css';
 import Meh from "./pages/Meh";
+//import Home from "./pages/home";
+//import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 function App() {
-
+  
   return (
     <div className="App">
       <Meh/>
@@ -12,64 +14,4 @@ function App() {
 }
 
 export default App;
-*/
 
-import React, {useState, useEffect} from "react";
-import firebase from "./firebase";
-
-//import {firestore} from "../firebase";
-//import {querySnapshot, collection} from "@firebase/firestore"; 
-
-function Meh(){
-    const [sales, setSales] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    const ref = firebase.firestore().collection("Sales"); //collection(firestore, "Sales");
-        
-    useEffect(() => {
-      function getSales(){
-        setLoading(true);
-        ref.onSnapshot((querySnapshot) => {
-          const items = [];
-          querySnapshot.forEach((doc) => {
-            //console.log(doc.data());
-            items.push(doc.data());
-          });        
-          setSales(items);
-          //console.log(sales);
-          setLoading(false);
-        });
-      };
-
-      getSales(); 
-      //eslint-disable-next-line react-hooks/exhaustive-deps      
-    }, []);
-
-    if (loading){
-       return (
-        <h1>
-            Loading
-       </h1> 
-       )
-    };
-
-    return(
-        <div>
-            <h1 style = {{textAlign: "center"}}>Meh sales tracker</h1>
-            {sales.map((sale) =>(
-                <div style={{width:250 + "px"}} key = {sale.id}>
-                    <h3>
-                        Date of sale {sale.Date}
-                    </h3>
-                    <h3>Paid? {sale.Paid}</h3>
-                    <h3>Strain: {sale.Strain}</h3>
-                    <hr></hr>
-                </div>
-            ))}
-        </div>
-        
-    )
-
-};
-
-export default Meh;
--- src/pages/Meh.jsx
+++ src/pages/Meh.jsx
@@ -1,51 +1,75 @@
 import React, {useState, useEffect} from "react";
 import firebase from "../firebase";
+
 //import {firestore} from "../firebase";
 //import {querySnapshot, collection} from "@firebase/firestore"; 
 
-export default function Meh(){
+function Meh(){
     const [sales, setSales] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    const ref = firebase.firestore().collection("Sales"); //collection(firestore, "Sales");
-    
-    function getSales(){
+    const ref = firebase.firestore().collection("Sales");
+        
+    useEffect(() => {
+      function getSales(){
         setLoading(true);
         ref.onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) => {
-                items.push(doc.data());
-            });
-            setSales(items);
-            setLoading(false);
-        })
-    }; 
+          const items = [];
+          querySnapshot.forEach((doc) => {
+            items.push(doc.data());
+          });        
+          setSales(items);
+          setLoading(false);
+        });
+      };
 
-    useEffect(() => {
-        getSales();
+      getSales();     
     }, []);
 
-    if(loading){
-       return
+    if (loading){
+       return (
         <h1>
-            Loading.,.,
-       </h1> ;
-    }
+            Loading
+       </h1> 
+       )
+    };
 
     return(
         <div>
-            <h1>
-                Sales
-            </h1>
+            <h1 style = {{textAlign: "center"}}>Meh sales tracker</h1>
             {sales.map((sale) =>(
-                <div key = {sale.Strain}>
-                    <h2>
-                        {sale.date}
-                    </h2>
-                    <p>{sale.stat}</p>
+                <div style={{width:250 + "px"}} key = {sale.id}>
+                    <h3>
+                        Date of sale {sale.Date}
+                    </h3>
+                    <h3>Paid? {sale.Paid}</h3>
+                    <h3>Strain: {sale.Strain}</h3>
+                    <hr></hr>
                 </div>
             ))}
-        </div>
-    );
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <footer>
+            To-Do:
+                <h5>
+                    Realtime stocks update<br></br>
+                    Payment security<br></br>
+                    Pre-order option when stocks are low<br></br>
+                    Drone logistics<br></br>
+                    Name change<br></br>
+                    Customer loyalty (number of purchases)<br></br>
+                    Diversify products<br></br>
+                    UI/UX<br></br>
+                </h5>
+        </footer>
+        </div>        
+    )
 
 };
+
+export default Meh;
